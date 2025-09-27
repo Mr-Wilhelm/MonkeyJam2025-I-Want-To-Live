c73a6c224040899c6d62932e2e74e6b21e111121
Edward Hayden <edwardhayden04@gmail.com>
2025-09-27 22:11:39 +0100

    Added placeholder art and dragging mechanic

---
+++ Scripts/DraggableArea.gd
@@ -0,0 +1,31 @@
+extends Area2D
+
+var dragging : bool = false
+var drag_offset : Vector2 = Vector2.ZERO
+
+func _input_event(viewport: Viewport, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.pressed:
+			dragging = true
+			drag_offset = get_parent().global_position - get_global_mouse_position()
+		else:
+			dragging = false
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion and dragging:
+		var lower_bounds = get_viewport_rect().get_center() - get_viewport_rect().size/2
+		var upper_bounds = get_viewport_rect().get_center() + get_viewport_rect().size/2
+		var deadzone_offset = 50
+		
+		get_parent().global_position = get_global_mouse_position() + drag_offset
+		print("A: " + str(get_parent().global_position.x))
+		print("A: " + str((lower_bounds.x)))
+		if get_parent().global_position.x < lower_bounds.x + deadzone_offset:
+			get_parent().global_position.x = lower_bounds.x + deadzone_offset
+		elif get_parent().global_position.x > upper_bounds.x - deadzone_offset:
+			get_parent().global_position.x = upper_bounds.x - deadzone_offset
+		if get_parent().global_position.y < lower_bounds.y + deadzone_offset:
+			get_parent().global_position.y = lower_bounds.y + deadzone_offset
+		elif get_parent().global_position.y > upper_bounds.y - deadzone_offset:
+			get_parent().global_position.y = upper_bounds.y - deadzone_offset
+		
