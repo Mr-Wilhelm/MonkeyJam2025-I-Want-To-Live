d6f91d269c638b23da40ac88fc90dfd8d2a0010c
Will Andrew <willandrew3141@gmail.com>
2025-09-27 13:50:20 +0100

    most interaction stuff done, fixing issue with overlay canvas

---
+++ Prefabs/InteractLogic.gd
@@ -0,0 +1,39 @@
+extends Node2D
+
+@onready var interactLable: Label = $Label
+var currentInteractions := []	#list of interactable objects
+var canInteract := true
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("Interact") and canInteract:
+		if currentInteractions:		#checks if there are interactions around
+			canInteract = false
+			interactLable.hide()
+			
+			await currentInteractions[0].interact.call()	#calls the interact function of the object
+			
+			canInteract = true
+
+func _process(_delta: float) -> void:
+	if currentInteractions and canInteract:
+		currentInteractions.sort_custom(SortByNearest)	#gets the nearest interactable
+		if currentInteractions[0].canInteract:
+			#interactLable.text = currentInteractions[0].interactName	
+			interactLable.show()
+	else:
+		interactLable.hide()
+
+func SortByNearest(area1, area2):
+	var area1Distance = global_position.distance_to(area1.global_position)
+	var area2Distance = global_position.distance_to(area2.global_position)
+	
+	return area1Distance < area2Distance
+
+func _on_interact_range_area_entered(area: Area2D) -> void:
+	print("Entered", area)
+	currentInteractions.push_back(area)
+
+
+func _on_interact_range_area_exited(area: Area2D) -> void:
+	print("Exited", area)
+	currentInteractions.erase(area)
---
+++ Prefabs/interactable.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+@export var interactName: String = ""
+@export var canInteract: bool = true
+
+var interact: Callable = func():
+	pass
