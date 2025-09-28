931d34b8c4cea5fe6c921b8395f36a9ff9101dca
Will Andrew <willandrew3141@gmail.com>
2025-09-28 17:37:12 +0100

    footstep audio working and synced

--- Scripts/PlayerMovement.gd
+++ Scripts/PlayerMovement.gd
@@ -3,6 +3,12 @@ extends CharacterBody2D
 @export var moveSpeed: float = 500
 var moveDir: Vector2
 
+@export var footstepSounds: Array[AudioStream]
+@onready var footstepPlayer: AudioStreamPlayer2D = $AudioStreamPlayer2D
+
+@export var footstepInterval: float = 0.4 #time between footsteps
+var footstepTimer: float = 0.0
+
 func _physics_process(delta: float) -> void:
 	moveDir.x = Input.get_axis("MoveLeft", "MoveRight")
 	moveDir.y = Input.get_axis("MoveUp", "MoveDown")
@@ -10,6 +16,11 @@ func _physics_process(delta: float) -> void:
 	
 	if moveDir:
 		velocity = moveDir * moveSpeed
+		
+		footstepTimer -= delta
+		if footstepTimer <= 0.0:
+			PlayFootstepSound()
+			footstepTimer = footstepInterval
 		
 		if abs(moveDir.x) > abs(moveDir.y):
 			if moveDir.x > 0:
@@ -28,7 +39,15 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, moveSpeed)
 		if %Sprite.animation != "idle": %Sprite.animation = "idle"
+		footstepTimer = 0.0
 	move_and_slide()
+
+func PlayFootstepSound():
+	if footstepSounds.size() == 0:
+		return
+	var sound = footstepSounds.pick_random()
+	footstepPlayer.stream = sound
+	footstepPlayer.play()
 
 #currently set to semicolon (;) for the debug option, normally this function will be called from elsewhere
 func _input(event: InputEvent) -> void:
