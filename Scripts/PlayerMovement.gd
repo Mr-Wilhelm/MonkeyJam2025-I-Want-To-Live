5824866f1ab0486c04b16f263a5266d0f15fddbe
Will Andrew <willandrew3141@gmail.com>
2025-09-28 15:44:18 +0100

    player sprite added

--- Scripts/PlayerMovement.gd
+++ Scripts/PlayerMovement.gd
@@ -7,13 +7,24 @@ func _physics_process(delta: float) -> void:
 	moveDir.x = Input.get_axis("MoveLeft", "MoveRight")
 	moveDir.y = Input.get_axis("MoveUp", "MoveDown")
 	moveDir = moveDir.normalized()
-
-	if moveDir.x > 0: %Sprite.flip_h = false
-	elif moveDir.x < 0: %Sprite.flip_h = true
 	
 	if moveDir:
 		velocity = moveDir * moveSpeed
-		if %Sprite.animation != "walking": %Sprite.animation = "walking"
+		
+		if abs(moveDir.x) > abs(moveDir.y):
+			if moveDir.x > 0:
+				if %Sprite.animation != "WalkRight":
+					%Sprite.animation = "WalkRight"
+			else:
+				if %Sprite.animation != "WalkLeft":
+					%Sprite.animation = "WalkLeft"
+		else:
+			if moveDir.y > 0:
+				if %Sprite.animation != "WalkDown":
+					%Sprite.animation = "WalkDown"
+			else:
+				if %Sprite.animation != "WalkUp":
+					%Sprite.animation = "WalkUp"
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, moveSpeed)
 		if %Sprite.animation != "idle": %Sprite.animation = "idle"
